d22e9751094e95b59c0e8e8082961f573d2726f8
roberto.torres <ts.roberto@gmail.com>
2021-05-28 11:40:27 +0100

    emcc.sh and package.json scripts

--- build.sh
+++ build.sh
@@ -59,39 +59,3 @@ test -n "$SKIP_LIBWEBM" ||(
 echo "============================================="
 echo "Compiling libwebm done"
 echo "============================================="
-
-echo "============================================="
-echo "Compiling wasm bindings"
-echo "============================================="
-(
-  emcc \
-    ${OPTIMIZE} \
-    --bind \
-    -s STRICT=1 \
-    -s NO_DYNAMIC_EXECUTION=1 \
-    -s ALLOW_MEMORY_GROWTH=1 \
-    -s ASSERTIONS=0 \
-    -s MODULARIZE=1 \
-    -s FILESYSTEM=0 \
-    -s EXPORT_ES6=1 \
-    -s MALLOC=emmalloc \
-    --std=c++11 \
-    -I node_modules/libyuv/include \
-    -I node_modules/libvpx \
-    -I node_modules/libwebm \
-    -I build-vpx \
-    -I build-webm \
-    -o ./webm-wasm.js \
-    -x c++ \
-    src/webm-wasm.cpp \
-    src/mkvwriter/mymkvwriter.cpp \
-    src/mkvwriter/mymkvstreamwriter.cpp \
-    build-yuv/libyuv.a \
-    build-vpx/libvpx.a \
-    build-webm/libwebm.a
-  mkdir dist || true
-  mv webm-wasm.{js,wasm} dist
-)
-echo "============================================="
-echo "Compiling wasm bindings done"
-echo "============================================="
---
+++ emcc.sh
@@ -0,0 +1,32 @@
+echo "============================================="
+echo "Compiling wasm bindings"
+echo "============================================="
+(
+  emcc \
+    ${OPTIMIZE} \
+    --bind \
+    -s STRICT=1 \
+    -s ALLOW_MEMORY_GROWTH=1 \
+    -s ASSERTIONS=0 \
+    -s MODULARIZE=1 \
+    -s FILESYSTEM=0 \
+    -s EXPORT_ES6=1 \
+    -s MALLOC=emmalloc \
+    --std=c++11 \
+    -I node_modules/libyuv/include \
+    -I node_modules/libvpx \
+    -I node_modules/libwebm \
+    -I build-vpx \
+    -I build-webm \
+    -o ./webm-wasm.js \
+    -x c++ \
+    src/webm-wasm.cpp \
+    src/mkvwriter/mymkvwriter.cpp \
+    src/mkvwriter/mymkvstreamwriter.cpp \
+    build-yuv/libyuv.a \
+    build-vpx/libvpx.a \
+    build-webm/libwebm.a
+)
+echo "============================================="
+echo "Compiling wasm bindings done"
+echo "============================================="
